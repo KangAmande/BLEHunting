cc1c607295d62c47ea278640999c7b7be501d35a
Amandeep Kaur <amankang344@gmail.com>
2026-03-11 21:50:35 -0400

    Added a couple more characteristics of device data

--- radar.js
+++ radar.js
@@ -10,12 +10,27 @@ noble.on('stateChange', async(state) => {
 });
 noble.on('discover', (peripheral) => {
     const name = peripheral.advertisement.localName || 'Unknown Peripheral';
+    const ID = peripheral.id || 'Unknown ID';
     const rssi = peripheral.rssi;
     const address = peripheral.address !== 'unknown' ? peripheral.address : peripheral.id;
     const privacyLeak = peripheral.advertisement.localName ? 'Data Leak : Name Public' : 'Private';
+    const mfgData = peripheral.advertisement.manufacturerData;
+    let mfgName = "Unknown Manufacturer";
+    
+    // Check if mfgData exists AND has at least 2 bytes before reading
+    if (Buffer.isBuffer(mfgData) && mfgData.length >= 2) {
+      const code = mfgData.readUInt16LE(0); 
+      
+      if (code === 0x004C) mfgName = "Apple Inc.";
+      else if (code === 0x0059) mfgName = "Nordic Semiconductor";
+      else if (code === 0x0006) mfgName = "Microsoft";
+      else mfgName = `ID: 0x${code.toString(16).toUpperCase()}`;
+    }
     console.log('------------------------------------------------------------');
     console.log(`Device Name: ${name}`);
+    console.log(`ID: ${ID}`);
     console.log(`Address: ${address}`);
     console.log(`RSSI: ${rssi} dBm`);
+    console.log(`Manufacturer Name: ${mfgName}`);
     console.log(`Privacy Leak: ${privacyLeak}`);
 });
